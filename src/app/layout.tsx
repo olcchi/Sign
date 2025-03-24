e69c133ab760d379b1e7860b29e48efe32479562
ekar <earthb0y@outlook.com>
2025-03-24 23:07:56 +0800

    feature : Meta icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/ui/themeProvider";
 import { Geist, Geist_Mono, DM_Serif_Text } from "next/font/google";
@@ -21,6 +22,9 @@ const dmSerifText = DM_Serif_Text({
 
 export const metadata: Metadata = {
   title: "Soul Sign ",
+  icons: {
+    icon: "/butterfly.svg",
+  },
   description: "Generate your electronic cheering sign",
 };
 
@@ -34,9 +38,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${dmSerifText.variable} antialiased`}
       >
+        {" "}
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
