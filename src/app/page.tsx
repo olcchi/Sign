c57ddbf345677448044df805c0119844e57ff6fa
ekar <earthb0y@outlook.com>
2025-03-24 23:10:08 +0800

    feature : Main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,18 @@
 "use client";
-import Panel from "@/components/panel";
-import CanvasDots from "@/components/canvasDots";
-import AnimationText from "@/components/circularText";
-
-import { motion } from "motion/react";
+import FullScreen from "@/components/ui/fullScreen";
+import EnterPageContent from "@/components/ui/enterPageContent";
+import NoisyButterflyBackground from "@/components/ui/noisyButterflyBackground/noisyButterflyBackground";
 import { useRef } from "react";
 export default function Home() {
   const dragContainer = useRef(null);
   return (
     <main
       ref={dragContainer}
-      className="relative w-screen h-screen overflow-clip font-[family-name:var(--font-dm-serif-text)] italic"
+      className="relative w-screen h-[100dvh] overflow-clip flex justify-center items-center"
     >
-      <Panel />
-      <main className="w-full h-full flex justify-center items-center">
-        <CanvasDots />
-        <motion.div
-          drag={true}
-          dragMomentum={false}
-          dragConstraints={dragContainer}
-          whileTap={{ scale: 0.9 }}
-          className="rounded-full"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 100 }}
-          transition={{ duration: 1 }}
-        >
-          <AnimationText
-            text="DT IN THE HOUSE !"
-            onHover="speedUp"
-            spinDuration={100}
-            className="w-100! h-100! text-5xl rounded-full"
-          />
-        </motion.div>
-      </main>
+      <NoisyButterflyBackground />
+      <FullScreen className="fixed top-4 right-4" />
+      <EnterPageContent />
     </main>
   );
 }
