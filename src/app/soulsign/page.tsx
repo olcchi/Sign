3c984ba6a18fb33fb0fb9f2c2eade395d5eabf49
ekar <earthb0y@outlook.com>
2025-03-27 22:53:25 +0800

    feat : Sign page

--- src/app/soulsign/page.tsx
+++ src/app/soulsign/page.tsx
@@ -2,26 +2,24 @@
 import CanvasDots from "@/components/ui/canvasDots";
 import CircularText from "@/components/ui/widgets/circularText";
 import FullScreen from "@/components/ui/fullScreen";
-import { EditWrapper } from "@/components/ui/widgets/editableWrapper";
+import EnterPageContent from "@/components/ui/SetupGuide/setupGuide";
+import { EditWrapper } from "@/components/ui/widgets/editableWrapper/editableWrapper";
+import ToolBar from "@/components/ui/toolBar";
+import ShinyText from "@/components/ui/shinyText/shinyText";
+import '@/components/ui/shinyText/shinyText.css'
+import ScrollingText from "@/components/ui/widgets/scrollingText/scrollingText";
 import { useRef } from "react";
 export default function Home() {
   const dragContainer = useRef(null);
   return (
     <main
       ref={dragContainer}
-      className="relative w-screen h-screen overflow-clip font-[family-name:var(--font-dm-serif-text)] italic"
+      className="relative w-screen h-screen overflow-hidden font-[family-name:var(--font-dm-serif-text)] italic"
     >
-      {/* <VerticalScreenMask></VerticalScreenMask> */}
+      <EnterPageContent />
       <main className="w-full h-full flex justify-center items-center">
-        <CanvasDots />
-        <EditWrapper elementRef={dragContainer}>
-          <CircularText
-            text="DT IN THE HOUSE !"
-            onHover="speedUp"
-            spinDuration={100}
-            className="h-60! w-60! text-5xl rounded-full"
-          />
-        </EditWrapper>
+        <ScrollingText className="font-4xl" speed={300} editable={true} text='dt in the house' />
+        {/* <ToolBar /> */}
         <FullScreen  className="fixed top-4 right-4"/>
       </main>
     </main>
