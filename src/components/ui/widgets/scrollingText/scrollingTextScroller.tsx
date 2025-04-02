d834fc9af1913d2edb674bc7775829fff580429e
ekar <earthb0y@outlook.com>
2025-04-02 23:41:36 +0800

    chore : file path

---
+++ src/components/ui/widgets/scrollingText/scrollingTextScroller.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { motion } from "motion/react";
+import "./scrollingTextScroller.css";
+interface ScrollingTextScrollerProps {
+  textWidth: number;
+  TEXT_GAP: number;
+  animationDuration: number;
+  textStyle: React.CSSProperties;
+  editableText: string;
+  textRef: React.RefObject<HTMLDivElement>;
+}
+
+export const ScrollingTextScroller: React.FC<ScrollingTextScrollerProps> = ({
+  textWidth,
+  TEXT_GAP,
+  animationDuration,
+  textStyle,
+  editableText,
+  textRef,
+}) => {
+  return (
+    <motion.div
+      className="flex whitespace-nowrap"
+      style={{ ...textStyle, gap: `${TEXT_GAP}px` }}
+      animate={{
+        x: [`0%`, `-${textWidth + TEXT_GAP}px`],
+      }}
+      transition={{
+        x: {
+          repeat: Infinity,
+          repeatType: "loop",
+          duration: animationDuration,
+          ease: "linear",
+        },
+      }}
+    >
+      <div
+        ref={textRef}
+        className="whitespace-nowrap inline-block select-none"
+      >
+        {editableText}
+      </div>
+      <div className="whitespace-nowrap inline-block select-none">
+        {editableText}
+      </div>
+    </motion.div>
+  );
+};
