eadc1560288fb041b66191bbb0b85b976cd26ed6
ekar <earthb0y@outlook.com>
2025-05-09 20:01:01 +0800

    feat: Integrate Olcchi component for author watermark display and manage GitHub visibility state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 "use client";
-import FullScreen from "@/components/ui/layout/fullScreen";
 import EnterPageContent from "@/components/ui/setup/SetupGuide/setupGuide";
 import NoisyButterflyBackground from "@/components/ui/noisyButterflyBackground/noisyButterflyBackground";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import {Olcchi} from "@/components/ui/olcchi";
 export default function Home() {
   const dragContainer = useRef(null);
+  const [showGithub, setShowGithub] = useState(false);
   return (
     <main
       ref={dragContainer}
       className="relative w-screen h-[100dvh] overflow-clip flex justify-center items-center"
     >
       <NoisyButterflyBackground />
-      <FullScreen className="fixed top-4 right-4" />
       <EnterPageContent />
+      {/* Author watermark */}
+      <Olcchi
+        id="olcchi"
+        personalUrl="https://olcchi.me"
+        githubUrl="https://github.com/soulsign"
+        showGithub={showGithub}
+      />
     </main>
   );
 }
