d4582f1a83433caa70709f18132582a7453f533e
ekar <earthb0y@outlook.com>
2025-06-03 22:52:40 +0800

    refactor: Enhance layout and structure of FontSizeSetting component for better user experience

--- src/components/ui/settings/settingsComponent/FontSizeSetting.tsx
+++ src/components/ui/settings/settingsComponent/FontSizeSetting.tsx
@@ -14,15 +14,19 @@ interface FontSizeSettingProps {
 export function FontSizeSetting({ fontSizeOptions }: FontSizeSettingProps) {
   const { textSettings, updateTextSettings } = useSettings();
 
-  return {
-    title: "字体尺寸",
-    component: (
-      <OptionButtonGroup
-        options={fontSizeOptions}
-        selectedValue={textSettings.fontSize}
-        buttonSize="icon"
-        onChange={(size) => updateTextSettings({ fontSize: size })}
-      />
-    ),
-  };
+  return (
+    <div className="border-b overflow-hidden">
+      <div className="flex justify-between items-center p-2">
+        <span className="text-sm">尺寸</span>
+      </div>
+      <div className="p-2">
+        <OptionButtonGroup
+          options={fontSizeOptions}
+          selectedValue={textSettings.fontSize}
+          buttonSize="icon"
+          onChange={(size) => updateTextSettings({ fontSize: size })}
+        />
+      </div>
+    </div>
+  );
 } 
