8a52be799ac52f44a6e5629904c25101d660f528
ekar <earthb0y@outlook.com>
2025-06-26 01:32:18 +0800

    refactor: Revamp AnimatedShare component for improved text display
    - Replaced Braces and motion elements with TextPressure for enhanced text rendering.
    - Updated TextLoop to display dynamic text with a larger font size and adjusted styling for better visibility.
    - Streamlined the component structure for improved readability and maintainability.

--- src/components/ui/welcome/animated-share.tsx
+++ src/components/ui/welcome/animated-share.tsx
@@ -1,44 +1,26 @@
-import { TextMorph } from "./morph-text";
-import { useState, useEffect } from "react";
-import { Cursor } from "./icon/cursor";
-import { LoadingCircle } from "./icon/loading-circle";
-import { motion } from "motion/react";
-import { Button } from "@/components/ui/layout/button";
-import { Braces } from "./icon/braces";
-import { ArrowRight } from "lucide-react";
-import { Star } from "@/components/ui/welcome/icon/star";
+import TextPressure from "@/components/ui/welcome/text-pressure";
 import { TextLoop } from "./text-loop";
 function AnimatedShare() {
   return (
     <div className="relative w-full h-full flex items-center justify-center">
-      <Braces className="">
-        <div className="relative flex flex-col items-center justify-center">
-          {/* <motion.div
-        initial={{ rotate: 0 }}
-        animate={{ rotate: 360 }}
-        transition={{
-          duration: 4,
-          repeat: Infinity,
-          ease: "easeInOut",
-          repeatDelay: 1,
-        }}
-        className="absolute -top-10 left-5"
-        style={{
-          transformOrigin: "center center",
-        }}
-      >
-        <Star className=" w-10 h-10" />
-      </motion.div> */}
-          <div className="flex items-center">
-            <TextLoop className="text-[64px] font-bold">
-              <p>生成</p>
-              <p>分享</p>
-              <p>加载</p>
-            </TextLoop>
-            <p className="text-white text-[64px] font-bold ml-2">PIN码</p>
-          </div>
-        </div>
-      </Braces>
+      <TextPressure
+        className="w-full"
+        text="{PIN Code}"
+        flex={true}
+        alpha={false}
+        stroke={false}
+        width={true}
+        weight={true}
+        italic={false}
+        minFontSize={120}
+      />
+      <TextLoop
+      interval={3}
+      className="text-[256px] absolute text-[#211E55] opacity-70 -z-2">
+        <p>生成</p>
+        <p>分享</p>
+        <p>加载</p>
+      </TextLoop>
     </div>
   );
 }
