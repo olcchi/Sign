cc284f1171ab9a2b9d63e2cb1ea09c4567cf23a3
ekar <earthb0y@outlook.com>
2025-06-26 22:09:39 +0800

    feat: Update AnimatedShare component for improved text display
    - Wrapped TextPressure component in a div for better layout control.
    - Adjusted minFontSize from 120 to 36 for responsive design.
    - Added simulateMouseMovement, simulationPadding, and simulationSpeed properties to enhance text animation effects.

--- src/components/ui/welcome/animated-share.tsx
+++ src/components/ui/welcome/animated-share.tsx
@@ -3,20 +3,26 @@ import { TextLoop } from "./text-loop";
 function AnimatedShare() {
   return (
     <div className="relative w-full h-full flex items-center justify-center">
-      <TextPressure
-        className="w-full"
-        text="{PIN Code}"
-        flex={true}
-        alpha={false}
-        stroke={false}
-        width={true}
-        weight={true}
-        italic={false}
-        minFontSize={120}
-      />
+      <div className="h-fit w-full ">
+        <TextPressure
+          text="{PIN Code}"
+          flex={true}
+          alpha={false}
+          stroke={false}
+          width={true}
+          weight={true}
+          italic={false}
+          minFontSize={36}
+          simulateMouseMovement
+          simulationPadding={-100}
+          simulationSpeed={6000}
+        />
+      </div>
+
       <TextLoop
-      interval={3}
-      className="text-[256px] absolute text-[#211E55] opacity-70 -z-2">
+        interval={3}
+        className="text-[256px] absolute text-[#211E55] opacity-70 -z-2"
+      >
         <p>生成</p>
         <p>分享</p>
         <p>加载</p>
