67e5910921f400a0f404f4b2d14d35fa9691907c
ekar <earthb0y@outlook.com>
2025-07-02 21:08:24 +0800

    refactor(text-fill-setting): update import path for Select components

--- src/components/ui/settings/settings-component/text-fill-setting.tsx
+++ src/components/ui/settings/settings-component/text-fill-setting.tsx
@@ -8,7 +8,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/inputs/select";
+} from "@/components/ui/inputs";
 
 interface TextFillSettingProps {
   colorOptions: Array<{
