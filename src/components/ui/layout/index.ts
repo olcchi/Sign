0e271e5277d186c39ba43825a1478e81cc9a6b68
ekar <earthb0y@outlook.com>
2025-07-02 21:04:04 +0800

    refactor(layout): create index file to consolidate exports for UI components

---
+++ src/components/ui/layout/index.ts
@@ -0,0 +1,43 @@
+import FullScreen from "./full-screen";
+import Dialog, { DialogPortal, DialogOverlay, DialogTrigger, DialogClose, DialogContent, DialogHeader, DialogFooter, DialogTitle, DialogDescription } from "./dialog";
+import Switch from "./switch";
+import Label from "./label";
+import Card, { CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from "./card";
+import Alert, { AlertTitle, AlertDescription } from "./alert";
+import Separator from "./separator";
+import { ThemeProvider } from "./theme-provider";
+import Button, { buttonVariants } from "./button";
+import Accordion, { AccordionItem, AccordionTrigger, AccordionContent } from "./accordion";
+
+export {
+  FullScreen,
+  Dialog,
+  DialogPortal,
+  DialogOverlay,
+  DialogTrigger,
+  DialogClose,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+  Switch,
+  Label,
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  Alert,
+  AlertTitle,
+  AlertDescription,
+  Separator,
+  ThemeProvider,
+  Button,
+  buttonVariants,
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent
+}; 
