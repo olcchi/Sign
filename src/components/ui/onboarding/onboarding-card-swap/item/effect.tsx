ad57e16503c837eba888dc1acbf7f392d9f4a784
ekar <earthb0y@outlook.com>
2025-07-04 19:16:06 +0800

    feat(effect): add visual effects including EdgeBlur, Noise, and StarField to enhance onboarding experience

--- src/components/ui/onboarding/onboarding-card-swap/item/effect.tsx
+++ src/components/ui/onboarding/onboarding-card-swap/item/effect.tsx
@@ -1,5 +1,34 @@
+import { EdgeBlurEffect, Noise, StarField } from "@/components/ui/filter";
+
+
 function Effect() {
-  return <div>Effect</div>;
+  return (
+    <div className="relative w-full h-full">
+      {/* <TextRoll
+        duration={0.5}
+        infinite={true}
+        infiniteDelay={0.5}
+        className="absolute flex justify-center items-center text-transparent text-[128px] text-center inset-0 z-1 [-webkit-text-stroke:1px_white] [text-stroke:1px_white]"
+      >
+        Effect
+      </TextRoll> */}
+      <div className="absolute inset-0 z-2">
+        <EdgeBlurEffect enabled intensity={10} />
+      </div>
+      <div className="absolute inset-0 z-3">
+        <Noise />
+      </div>
+      <div className="absolute inset-0 z-1">
+        <StarField
+          enabled={true}
+          size={4}
+          twinkleSpeed={3}
+          color="#ffffff"
+          density={50}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Effect;
