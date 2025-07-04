357a031f1f4932891cf4e9c4991b55959408619c
ekar <earthb0y@outlook.com>
2025-07-04 19:16:01 +0800

    feat(version-footer): add VersionFooter component to display version information in onboarding

---
+++ src/components/ui/onboarding/version-footer.tsx
@@ -0,0 +1,7 @@
+export function VersionFooter() {
+  return (
+    <div className="flex h-4 justify-between items-center fixed bottom-4 left-4 z-20">
+      <p className="text-xs  opacity-30">Version Alpha</p>
+    </div>
+  );
+}
