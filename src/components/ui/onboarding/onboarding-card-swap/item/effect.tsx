c063375039744d8703cfeba6e8bdcac82c9f92cd
ekar <earthb0y@outlook.com>
2025-07-05 21:07:04 +0800

    refactor(ui): update TextRoll component properties in Effect for improved animation timing and styling

--- src/components/ui/onboarding/onboarding-card-swap/item/effect.tsx
+++ src/components/ui/onboarding/onboarding-card-swap/item/effect.tsx
@@ -1,17 +1,17 @@
 import { EdgeBlurEffect, Noise, StarField } from "@/components/ui/filter";
-
+import { TextRoll } from "@/components/ui/onboarding";
 
 function Effect() {
   return (
     <div className="relative w-full h-full">
-      {/* <TextRoll
-        duration={0.5}
+      <TextRoll
+        duration={1}
         infinite={true}
-        infiniteDelay={0.5}
-        className="absolute flex justify-center items-center text-transparent text-[128px] text-center inset-0 z-1 [-webkit-text-stroke:1px_white] [text-stroke:1px_white]"
+        infiniteDelay={2}
+        className=" absolute flex justify-center items-center text-transparent text-[128px] text-center inset-0 z-1 [-webkit-text-stroke:1px_#F4F2F8] [text-stroke:1px_#212121]"
       >
         Effect
-      </TextRoll> */}
+      </TextRoll>
       <div className="absolute inset-0 z-2">
         <EdgeBlurEffect enabled intensity={10} />
       </div>
