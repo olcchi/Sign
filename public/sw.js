b78bbeba02fd9509691f669e32e92645b959e404
ekar <earthb0y@outlook.com>
2025-07-07 01:03:41 +0800

    refactor(pwa): remove unused service worker event listeners to streamline code

--- public/sw.js
+++ public/sw.js
@@ -19,27 +19,4 @@ self.addEventListener('notificationclick', function (event) {
   console.log('Notification click received.')
   event.notification.close()
   event.waitUntil(clients.openWindow(self.location.origin))
-})
-
-// Install event - cache important resources
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sign-v1').then(function(cache) {
-      return cache.addAll([
-        '/',
-        '/Sign-Logo.png',
-        '/sign-light.svg',
-        '/sign-dark.svg'
-      ])
-    })
-  )
-})
-
-// Fetch event - serve from cache when offline
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request)
-    })
-  )
 }) 
