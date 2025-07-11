aa1f3c7c3096739b3b8ec37c4cb27b968aa945e4
ekar <earthb0y@outlook.com>
2025-07-11 22:45:05 +0800

    fix(ui): adjust font size of version footer for better visibility

--- src/components/ui/onboarding/version-footer.tsx
+++ src/components/ui/onboarding/version-footer.tsx
@@ -1,7 +1,7 @@
 export function VersionFooter() {
   return (
     <div className="flex h-4 justify-between items-center fixed bottom-4 left-4 z-20">
-      <p className="text-xs  opacity-10">Alpha 0.1.0</p>
+      <p className="text-[10px] opacity-10">Alpha 0.1.0</p>
     </div>
   );
 }
